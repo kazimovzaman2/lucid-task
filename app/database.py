a368ce9c39bdcd101d5e4f25305b845b9f710a0a
kazimovzaman2 <kazimovzaman2@gmail.com>
2025-03-14 11:02:13 +0400

    Make .env example. Get db url from env

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+
+SQLALCHEMY_DATABASE_URL = config("DATABASE_URL", default="sqlite:///./test.db")
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
